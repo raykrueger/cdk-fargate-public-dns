034dc262b3eaaedb0e0779a5ce8ec236feb6e829
Ray Krueger <raykrueger@gmail.com>
2022-02-15 22:52:22 +0000

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Ray Krueger',
+  authorAddress: 'raykrueger@gmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-fargate-public-dns',
+  repositoryUrl: 'https://github.com/raykrueger/cdk-fargate-public-dns.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
