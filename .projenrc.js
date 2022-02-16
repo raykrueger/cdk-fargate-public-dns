1202c96d825a9b316012a4cf52e8d11234d61c2f
Ray Krueger <raykrueger@gmail.com>
2022-02-16 18:23:38 +0000

    Attempt npm releases

--- .projenrc.js
+++ .projenrc.js
@@ -7,6 +7,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   name: '@raykrueger/cdk-fargate-public-dns',
   repositoryUrl: 'https://github.com/raykrueger/cdk-fargate-public-dns.git',
   description: 'An AWS CDK module that listens for ECS Tasks to enter RUNNING state and then updates a Route 53 hosted zone with the public IP address attached to the task.',
+  releaseToNpm: true
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
