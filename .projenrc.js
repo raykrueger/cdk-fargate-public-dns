c108047c29759d21776015ac65839eed149353d8
Ray Krueger <raykrueger@gmail.com>
2022-08-16 02:35:32 +0000

    Still fixing @types/prettier

--- .projenrc.js
+++ .projenrc.js
@@ -14,7 +14,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: [
+    '@types/prettier'
+  ],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
