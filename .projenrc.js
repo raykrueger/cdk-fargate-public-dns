473c37c5d0d0df225084eb873074075fe0b55e65
Ray Krueger <raykrueger@gmail.com>
2022-08-15 21:53:35 -0500

    Still fixing this garbage

--- .projenrc.js
+++ .projenrc.js
@@ -15,7 +15,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: [
-    '@types/prettier@2.6.0'
+    '@types/prettier@2.6.0',
   ],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
