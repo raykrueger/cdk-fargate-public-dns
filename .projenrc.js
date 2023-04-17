b37160b1e545f5e1e75f736efe2ef31fe342519d
Ray Krueger <raykrueger@gmail.com>
2023-04-17 09:30:47 +0000

    Upgrade dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -2,13 +2,13 @@ const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Ray Krueger',
   authorAddress: 'raykrueger@gmail.com',
-  cdkVersion: '2.69.0',
+  cdkVersion: '2.74.0',
   defaultReleaseBranch: 'main',
   name: '@raykrueger/cdk-fargate-public-dns',
   repositoryUrl: 'https://github.com/raykrueger/cdk-fargate-public-dns.git',
   description: 'An AWS CDK module that listens for ECS Tasks to enter RUNNING state and then updates a Route 53 hosted zone with the public IP address attached to the task.',
   releaseToNpm: true,
-  minNodeVersion: '16.13.0',
+  minNodeVersion: '16.14.0',
   catalog: {
     announce: false,
     twitter: 'raykrueger',
